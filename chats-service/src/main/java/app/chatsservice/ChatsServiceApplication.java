64db3351b1cfe74c8c55f6cf6d5737dc0a2a726e
LongPT <p.thelong2000@gmail.com>
2024-01-03 20:52:22 +0700

    create chat service

---
+++ chats-service/src/main/java/app/chatsservice/ChatsServiceApplication.java
@@ -0,0 +1,13 @@
+package app.chatsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatsServiceApplication.class, args);
+	}
+
+}
---
+++ chats-service/src/test/java/app/chatsservice/ChatsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package app.chatsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
