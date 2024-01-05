27cbb36bbb5727275340414cf7040a6a6e5dd452
LongPT <p.thelong2000@gmail.com>
2024-01-05 22:14:14 +0700

    [Chats_Service]: add repository

---
+++ chats-service/src/main/java/app/chatsservice/repository/impl/ConversationMemberCustomRepositoryImpl.java
@@ -0,0 +1,6 @@
+package app.chatsservice.repository.impl;
+
+import app.chatsservice.repository.ConversationMemberCustomRepository;
+
+public class ConversationMemberCustomRepositoryImpl implements ConversationMemberCustomRepository {
+}
