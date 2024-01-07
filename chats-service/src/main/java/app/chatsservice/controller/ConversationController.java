5690f6691d727c7639fe7a47aeb8408a314b6dc6
LongPT <p.thelong2000@gmail.com>
2024-01-07 20:09:02 +0700

    [Chats_Service]: create controller

--- chats-service/src/main/java/app/chatsservice/controller/ConversationController.java
+++ chats-service/src/main/java/app/chatsservice/controller/ConversationController.java
@@ -1,4 +1,26 @@
 package app.chatsservice.controller;
 
+import app.chatsservice.dto.response.ConversationResponse;
+import app.chatsservice.service.ConversationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/conversations")
 public class ConversationController {
+
+    private final ConversationService conversationService;
+
+    @GetMapping("/{conversationId}")
+    public ResponseEntity<ConversationResponse> getConversationById(
+            @PathVariable("conversationId") Long conversationId) {
+        return ResponseEntity.ok(conversationService.getConversationById(conversationId));
+    }
+
+    @PostMapping("/create/{userId}")
+    public ResponseEntity<ConversationResponse> createConversation() {
+        return null;
+    }
 }
