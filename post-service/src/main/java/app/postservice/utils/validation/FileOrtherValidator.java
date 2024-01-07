e8e5c95ecd211f10479d28a6a43e2ae996445575
daotrongthuan1010 <conxinloime1010@gmail.com>
2024-01-07 22:45:45 +0700

    [Post_Service]: Update service for post service and handler file

---
+++ post-service/src/main/java/app/postservice/utils/validation/FileOrtherValidator.java
@@ -0,0 +1,25 @@
+package app.postservice.utils.validation;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.Objects;
+
+public class FileOrtherValidator implements ConstraintValidator<FileOrther, MultipartFile> {
+
+    private static final long MAX_FILE_SIZE = 30 * 1024 * 1024;
+
+    @Override
+    public boolean isValid(MultipartFile file, ConstraintValidatorContext context) {
+        if (Objects.isNull(file)) {
+            return false;
+        }
+
+        long fileSize = file.getSize();
+        if (fileSize > MAX_FILE_SIZE) {
+            return false;
+        }
+        return true;
+    }
+}
