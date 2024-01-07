1f76de3115deded3ebf79302a8a5ec9ba4505105
daotrongthuan1010 <conxinloime1010@gmail.com>
2024-01-07 16:36:32 +0700

    [Social_Feed_Travel_Service]: Create new service social feed travel

---
+++ social-feed-travel-service/src/test/java/app/socialfeedtravelservice/SocialFeedTravelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package app.socialfeedtravelservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialFeedTravelServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
