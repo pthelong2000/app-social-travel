8e22dc351881a3717748541c94561bc136837997
daotrongthuan1010 <conxinloime1010@gmail.com>
2024-01-13 21:46:47 +0700

    [Post_Service]: Create stream for post service in kafka

--- post-service/src/main/java/app/postservice/service/PostArticleService.java
+++ post-service/src/main/java/app/postservice/service/PostArticleService.java
@@ -2,8 +2,11 @@ package app.postservice.service;
 
 import app.postservice.dto.request.CreatePostArticleRequest;
 import app.postservice.dto.request.UpdatePostArticleRequest;
+import app.postservice.entity.PostArticle;
 
 public interface PostArticleService {
+
+    PostArticle findPostArticleById(Long id);
 
     void savePostContent(CreatePostArticleRequest CreatePostArticleRequest);
 
--- post-service/src/main/java/app/postservice/utils/kafka/KafkaGroupIds.java
+++
@@ -1,6 +0,0 @@
-package app.postservice.utils.kafka;
-
-public interface KafkaGroupIds {
-    String COMMENT = "comments";
-    String NEW_FEED = "new-feed";
-}
