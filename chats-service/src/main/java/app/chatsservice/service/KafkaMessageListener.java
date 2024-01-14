3a4d8419b5405e0df4d17e9e580835b68364e0af
LongPT <p.thelong2000@gmail.com>
2024-01-14 14:52:31 +0700

    [Chats_Service]: create websocket

--- chats-service/src/main/java/app/chatsservice/service/KafkaMessageListener.java
+++ chats-service/src/main/java/app/chatsservice/service/KafkaMessageListener.java
@@ -1,15 +1,21 @@
 package app.chatsservice.service;
 
 import app.chatsservice.entity.Message;
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class KafkaMessageListener {
+
+    private final SimpMessagingTemplate simpMessagingTemplate;
 
     @KafkaListener(topics = "chat", groupId = "chats-service")
     public void listen(@Payload Message message) {
 
+        simpMessagingTemplate.convertAndSend("/message/" + message.getConversationId(), message);
     }
 }
