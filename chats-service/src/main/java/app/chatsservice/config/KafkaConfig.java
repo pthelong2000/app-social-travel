230c0eb3429a3c2382c98f5ed455fa73eb37225b
LongPT <p.thelong2000@gmail.com>
2024-01-14 10:19:26 +0700

    [Chats_Service]: update yml and config of kafka

--- chats-service/src/main/java/app/chatsservice/config/KafkaConfig.java
+++ chats-service/src/main/java/app/chatsservice/config/KafkaConfig.java
@@ -1,4 +1,18 @@
 package app.chatsservice.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
 public class KafkaConfig {
+
+    @Bean
+    public NewTopic chatTopic() {
+        return TopicBuilder.name("chat")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
 }
