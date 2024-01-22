ae750f43662ef5ec2b4d2997872ba820638139c6
LongPT <p.thelong2000@gmail.com>
2024-01-22 20:56:17 +0700

    [Chats_Service]: update

--- chats-service/src/main/java/app/chatsservice/exception/handler/ChatServiceHandleException.java
+++ chats-service/src/main/java/app/chatsservice/exception/handler/ChatServiceHandleException.java
@@ -3,10 +3,8 @@ package app.chatsservice.exception.handler;
 import app.chatsservice.exception.custom.ChatServiceCustomException;
 import app.chatsservice.exception.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
